ca1a4c45828874ea38f58607f6b02ca635e5c6ed
GWartelle <115559211+GWartelle@users.noreply.github.com>
2023-11-12 18:46:26 +0100

    ajout d'un timer pour la planche

--- chat.gd
+++ chat.gd
@@ -5,25 +5,25 @@ var raft_size # Taille de la fenêtre
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#raft_size = get_canvas_item()
-	#salut je suis wooly
-	pass
+    #raft_size = get_canvas_item()
+    #salut je suis wooly
+    pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("move_right"):
-		velocity.x +=1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -=1
-	if Input.is_action_pressed("move_down"):
-		velocity.y +=1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -=1
-		
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		
-	position += velocity * delta
-	#position = position.clamp(Vector2.ZERO, raft_size)
+    var velocity = Vector2.ZERO
+    if Input.is_action_pressed("move_right"):
+        velocity.x +=1
+    if Input.is_action_pressed("move_left"):
+        velocity.x -=1
+    if Input.is_action_pressed("move_down"):
+        velocity.y +=1
+    if Input.is_action_pressed("move_up"):
+        velocity.y -=1
+        
+    if velocity.length() > 0:
+        velocity = velocity.normalized() * speed
+        
+    position += velocity * delta
+    #position = position.clamp(Vector2.ZERO, raft_size)
