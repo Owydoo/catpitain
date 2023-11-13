442765efdd8553bc67842b0fa6cf36eaddff96ec
Nostalsheep <nostalsheep@gmail.com>
2023-11-13 17:11:33 +0100

    OK CA MARCHE
    Chat qui bouge + collisions + mât

--- main.gd
+++ main.gd
@@ -8,4 +8,19 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if self.isCatPositionInFront() == true :
+		$chat.z_index = $Radeau/Sprite2D_mat.z_index+1
+	else :
+		$chat.z_index = $Radeau/Sprite2D_mat.z_index-1
+		
+
+func isCatPositionInFront():
+#	if $chat.position.y > ($Radeau/Sprite2D_mat.position.y+($Radeau/Sprite2D_mat.texture.get_height()/2)):
+	if $chat.global_position.y > ($Radeau/matBottom.global_position.y):
+		print("chat :",$chat.global_position.y)
+		print("mat : ",$Radeau/matBottom.global_position.y)
+		return true
+	else : 
+		print("behind")
+		return false	
+	
