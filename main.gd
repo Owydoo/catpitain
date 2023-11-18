1fe7bedeb61bf258e3dc28dcae85038bcb08d4b2
Nostalsheep <nostalsheep@gmail.com>
2023-11-18 16:44:31 +0100

    petite opti

--- main.gd
+++ main.gd
@@ -1,26 +1,21 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimationPlayer.play("flotte")
+	$AnimationPlayer.play("flotte_radeau")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if self.isCatPositionInFront() == true :
-		$chat.z_index = $Radeau/Sprite2D_mat.z_index+1
+		$Node2D/chat.z_index = $Node2D/Radeau/Sprite2D_mat.z_index+1
 	else :
-		$chat.z_index = $Radeau/Sprite2D_mat.z_index-1
+		$Node2D/chat.z_index = $Node2D/Radeau/Sprite2D_mat.z_index-1
 		
 
 func isCatPositionInFront():
-#	if $chat.position.y > ($Radeau/Sprite2D_mat.position.y+($Radeau/Sprite2D_mat.texture.get_height()/2)):
-	if $chat.global_position.y > ($Radeau/matBottom.global_position.y):
-		print("chat :",$chat.global_position.y)
-		print("mat : ",$Radeau/matBottom.global_position.y)
+	if ($Node2D/chat.global_position.y+50) > ($Node2D/Radeau/matBottom.global_position.y):
 		return true
 	else : 
-		print("behind")
 		return false	
 	
